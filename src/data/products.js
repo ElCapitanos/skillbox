5d567578cd05cb5f3cb338337c033866c46a3c5d
Andrey Orlov <orlov75@gmail.com>
2020-11-07 18:34:04 +0300

    first

---
+++ src/data/products.js
@@ -0,0 +1,73 @@
+export default [
+    {
+        title: 'Гитара Fender Stratocaster',
+        price: 170000,
+        image: '/img/strat.jpg'
+    },
+
+    {
+        title: 'Гитара Fender Telecaster',
+        price: 172000,
+        image: '/img/tele.jpg'
+    },
+
+    {
+        title: 'Гитара Gibson ES-335',
+        price: 222000,
+        image: '/img/es.jpg'
+    },
+
+    {
+        title: 'Гитара Gibson Les Paul',
+        price: 180000,
+        image: '/img/les.jpg'
+    },
+
+    {
+        title: 'Гитара Gibson Flying V',
+        price: 99999,
+        image: '/img/fly.jpg'
+    },
+
+    {
+        title: 'Гитара Gretsch Falcon',
+        price: 320000,
+        image: '/img/falcon.jpg'
+    },
+
+    {
+        title: 'Гитара Music Man Axis',
+        price: 180000,
+        image: '/img/axis.jpg'
+    },
+
+    {
+        title: 'Гитара Peavey Wolfgang',
+        price: 99000,
+        image: '/img/peavey.jpg'
+    },
+
+    {
+        title: 'Гитара Jackson Soloist',
+        price: 129000,
+        image: '/img/jack.jpg'
+    },
+
+    {
+        title: 'Гитара Ibanez RG',
+        price: 89999,
+        image: '/img/rg.jpg'
+    },
+
+    {
+        title: 'Гитара Kramer Pacer',
+        price: 49999,
+        image: '/img/pacer.jpg'
+    },
+
+    {
+        title: 'Гитара PRS Santana',
+        price: 229999,
+        image: '/img/prs.jpg'
+    },
+]
---
+++ src/func.js
@@ -0,0 +1,10 @@
+import data from './data'
+
+function homework (text){
+    alert(text)
+  }
+
+  homework(data.message1)
+  homework(data.message2)
+
+  export default homework
