80dca7e567cac4b1fe858d5ff0654918ad627531
Andrey Orlov <orlov75@gmail.com>
2020-11-25 19:47:31 +0300

    4 module without ProductItem-colors

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,59 +1,85 @@
 <template>
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
+      <img :src="product.image" :alt="product.title" />
+    </a>
 
-          <li class="catalog__item">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image" :alt="product.title">
-            </a>
+    <h3 class="catalog__title">
+      <a href="#">
+        {{ product.title }}
+      </a>
+    </h3>
 
-            <h3 class="catalog__title">
-              <a href="#">
-                {{ product.title }}
-              </a>
-            </h3>
+    <span class="catalog__price"> {{ product.price }} ₽ </span>
 
-            <span class="catalog__price">
-              {{ product.price }} ₽
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" value="#222" v-model="color">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </li>
-
-
-
+    <ul class="colors colors--black">
+      <li class="colors__item" v-for="color in colors" :key="color.id">
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            name="color"
+            :value="color.id"
+            v-model.number="currentColorId"
+          />
+          <span
+            class="colors__value"
+            :style="{ 'background-color': color.code }"
+          >
+          </span>
+        </label>
+      </li>
+<!--
+      <li class="colors__item">
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            value="#73B6EA"
+            v-model="color"
+          />
+          <span class="colors__value" style="background-color: #73B6EA;">
+          </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            value="#8BE000"
+            v-model="color"
+          />
+          <span class="colors__value" style="background-color: #8BE000;">
+          </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            value="#222"
+            v-model="color"
+          />
+          <span class="colors__value" style="background-color: #222;"> </span>
+        </label>
+      </li>
+      -->
+    </ul>
+  </li>
 </template>
 
-
 <script>
+import colors from "../data/colors";
+import currentColorId from "./ProductFilter.vue";
 
 export default {
-    data() {
-        return {
-            color:'#73B6EA'
-        };
-    },
-    props: ['product']
-    }
-
+  data() {
+    return {
+      color: "#73B6EA",
+    };
+  },
+  props: ["product"],
+};
 </script>
--- src/data/colors.js
+++ src/data/colors.js
@@ -1,43 +1,43 @@
 export default [
-    {
-        id:1,
-        title: 'синий',
-        code: '#73B6EA'
-    },
+  {
+    id: 1,
+    title: "синий",
+    code: "#73B6EA",
+  },
 
-    {
-        id:2,
-        title: 'желтый',
-        code: '#FFBE15'
-    },
+  {
+    id: 2,
+    title: "желтый",
+    code: "#FFBE15",
+  },
 
-    {
-        id:3,
-        title: 'серый',
-        code: '#93939'
-    },
+  {
+    id: 3,
+    title: "серый",
+    code: "#737273",
+  },
 
-    {
-        id:4,
-        title: 'зеленый',
-        code: '#8BE000'
-    },
+  {
+    id: 4,
+    title: "зеленый",
+    code: "#8BE000",
+  },
 
-    {
-        id:5,
-        title: 'оранжевый',
-        code: '#FF6B00'
-    },
+  {
+    id: 5,
+    title: "оранжевый",
+    code: "#FF6B00",
+  },
 
-    {
-        id:6,
-        title: 'белый',
-        code: '#ffffff'
-    },
+  {
+    id: 6,
+    title: "белый",
+    code: "#ffffff",
+  },
 
-    {
-        id:7,
-        title: 'черный',
-        code: '#000000'
-    },
-]
+  {
+    id: 7,
+    title: "черный",
+    code: "#000000",
+  },
+];
