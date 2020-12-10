d3b0161f43cafa27882bb8c7df1144c581a0b877
Andrey Orlov <orlov75@gmail.com>
2020-12-10 18:43:38 +0300

    6 module 3 part

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -61,7 +61,12 @@
           {{ product.title }}
         </h2>
         <div class="item__form">
-          <form class="form" action="#" method="POST">
+          <form
+            class="form"
+            action="#"
+            method="POST"
+            @submit.prevent="addToCart"
+          >
             <b class="item__price"> {{ product.price | numberFormat }} ₽ </b>
 
             <fieldset class="form__block">
@@ -170,7 +175,7 @@
                   </svg>
                 </button>
 
-                <input type="text" value="1" name="count" />
+                <input type="text" v-model.number="productAmount" />
 
                 <button type="button" aria-label="Добавить один товар">
                   <svg width="12" height="12" fill="currentColor">
@@ -266,6 +271,12 @@ import gotoPage from "@/helpers/gotoPage";
 import numberFormat from "@/helpers/numberFormat";
 
 export default {
+  data() {
+    return {
+      productAmount: 1,
+    };
+  },
+
   /* props: ["pageParams"],*/
   filters: {
     numberFormat,
@@ -282,6 +293,12 @@ export default {
   },
   methods: {
     gotoPage,
+    addToCart() {
+      this.$store.commit("addProductToCart", {
+        productId: this.product.id,
+        amount: this.productAmount,
+      });
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        cartProducts: [
-            {productId:1, amount: 2}
-        ]
-    }
-});
+  state: {
+    cartProducts: [{ productId: 0, amount: 0 }],
+  },
+  mutations: {
+    addProductToCart(state, { productId, amount }) {
+      const item = state.cartProducts.find(
+        (item) => item.productId === productId
+      );
 
+      if (item) {
+        item.amount += amount;
+      } else {
+        state.cartProducts.push({
+          productId,
+          amount,
+        });
+      }
+    },
+  },
+});
